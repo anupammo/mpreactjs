ed008757d0b942516a51558c7a51ba0114782afd
Anupam Mondal <a09051985@gmail.com>
2024-05-11 03:36:23 +0530

    110520240336am

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="mpreactjs/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
