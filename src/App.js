6b227bb51533ae7c823b6ac1781fa878169d473f
Anupam Mondal <a09051985@gmail.com>
2024-05-11 03:21:19 +0530

    110520240321am

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-          
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App"></div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
-
-// import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 export default function App() {
