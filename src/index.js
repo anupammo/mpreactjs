13074b6be91949e20a96583307fed46b5cadce29
Anupam Mondal <a09051985@gmail.com>
2024-05-14 02:18:13 +0530

    140520240218am

--- src/index.js
+++ src/index.js
@@ -15,10 +15,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
